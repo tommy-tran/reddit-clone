abfdd234f3807a5083421d4f5614cb979d25e7e7
tommy-tran <trant28@mcmaster.ca>
2017-10-10 23:45:14 -0400

    Only open create post page if logged in

--- prototype/reddit-clone/src/pages/subreddit/subreddit.ts
+++ prototype/reddit-clone/src/pages/subreddit/subreddit.ts
@@ -1,88 +1,119 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, Navbar, Events, ModalController } from 'ionic-angular';
 import { Subreddit } from '../../models/subreddit.model';
-import { CreatePostPage } from "../../shared/pages";
+import { CreatePostPage, LoginPage } from "../../shared/pages";
 import { Post } from '../../models/post.model';
 import { DatabaseService } from '../../shared/database.service';
+import { AuthService } from '../../shared/auth.service';
 
 @IonicPage()
 @Component({
-  selector: 'page-subreddit',
-  templateUrl: 'subreddit.html',
+	selector: 'page-subreddit',
+	templateUrl: 'subreddit.html',
 })
 export class SubredditPage implements OnInit {
-  @ViewChild(Navbar) navBar: Navbar;
-  id : string;
-  subreddit: Subreddit;
-  posts: Post[];
-  times: string[];
+	@ViewChild(Navbar) navBar: Navbar;
+	id: string;
+	subreddit: Subreddit;
+	posts: Post[];
+	times: string[];
+	isLoggedIn: boolean;
 
-  // TODO: Description, possibly creator privileges, goToPost, voting
+	// TODO: Description, possibly creator privileges, goToPost, voting
 
-  constructor(
-    private databaseService: DatabaseService, 
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
-    public events: Events,
-    public modalCtrl: ModalController) {
-    this.subreddit = this.navParams.data;
-    this.id = this.subreddit.subreddit_id;
-    this.posts = [];
-    this.times = [];
-    this.databaseService.getSubredditPosts(this.id).then(posts => {
-      this.posts = Object.values(posts);
-      
-      // https://firebase.google.com/docs/reference/js/firebase.database.ServerValue for working with timestamps
+	constructor(
+		private databaseService: DatabaseService,
+		private authService: AuthService,
+		public navCtrl: NavController,
+		public navParams: NavParams,
+		public events: Events,
+		public modalCtrl: ModalController) {
+		this.isLoggedIn = this.authService.isLoggedIn();
+		this.subreddit = this.navParams.data;
+		this.id = this.subreddit.subreddit_id;
+		this.posts = [];
+		this.times = [];
+		this.getPosts();
 
-      // let currentDate = new Date();
-      // posts.forEach(post => {
-      //   let postDate = new Date(post.timestamp);
-      //   let timeDiff = this.calculateTimeDifference(postDate, currentDate)
-      //   this.times.push(timeDiff);
-      // });
-    });
+		// https://firebase.google.com/docs/reference/js/firebase.database.ServerValue for working with timestamps
+		// let currentDate = new Date();
+		// posts.forEach(post => {
+		//   let postDate = new Date(post.timestamp);
+		//   let timeDiff = this.calculateTimeDifference(postDate, currentDate)
+		//   this.times.push(timeDiff);
+		// });
 
-  }
+		this.events.subscribe('user:loggedin', () => {
+			this.authService.updateAuthState().then(() => {
+				this.posts = []; // Clear posts
+				this.getPosts(); // Get votable posts
+			});
+		});
 
-  ngOnInit() {
-    // When the back button is pressed
-    this.navBar.backButtonClick = () => {
-      this.events.publish('nav:subreddit');  
-      this.navCtrl.pop();
-    }
-  }
+		this.events.subscribe('user:loggedin', () => {
+			this.authService.updateAuthState().then(() => {
+				this.posts = []; // Clear posts
+				this.getPosts(); // Get votable posts
+			});
+		});
 
-  createPost() {
-    let createPostModal = this.modalCtrl.create(CreatePostPage, {subreddit: this.subreddit});
-    createPostModal.present();
-    // onWillDismiss()
-  }
-  
-  calculateTimeDifference(date1, date2) {
-    var difference = date1.getTime() - date2.getTime();
+	}
 
-    var daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
-    difference -= daysDifference * 1000 * 60 * 60 * 24
+	getPosts() {
+		this.databaseService.getSubredditPosts(this.id).then(posts => {
+			this.posts = Object.values(posts);
+		});
+	}
 
-    var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
-    difference -= hoursDifference * 1000 * 60 * 60
+	ngOnInit() {
+		// When the back button is pressed
+		this.navBar.backButtonClick = () => {
+			this.events.publish('nav:subreddit');
+			this.navCtrl.pop();
+		}
+	}
 
-    var minutesDifference = Math.floor(difference / 1000 / 60);
-    difference -= minutesDifference * 1000 * 60
+	createPost() {
+		if (this.isLoggedIn) {
+			let createPostModal = this.modalCtrl.create(CreatePostPage, { subreddit: this.subreddit });
+			createPostModal.present();
+		} else {
+			let authModal = this.modalCtrl.create(LoginPage);
+			authModal.present();
+			authModal.onWillDismiss((isLoggedIn) => {
+				if (isLoggedIn) {
+					this.isLoggedIn = isLoggedIn;
+				}
+			});
+		}
+		// onWillDismiss()
+	}
 
-    var secondsDifference = Math.floor(difference / 1000);
+	calculateTimeDifference(date1, date2) {
+		var difference = date1.getTime() - date2.getTime();
 
-    if (daysDifference >= 1) {
-      return daysDifference + ' d'
-    }
-    else if (hoursDifference >= 1 && hoursDifference < 24) {
-      return hoursDifference + ' hr'
-    }
-    else if (minutesDifference < 60 && minutesDifference >= 1) {
-      return minutesDifference + ' m';
-    }
-    else {
-      return secondsDifference + ' s'
-    }
-  }
+		var daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
+		difference -= daysDifference * 1000 * 60 * 60 * 24
+
+		var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
+		difference -= hoursDifference * 1000 * 60 * 60
+
+		var minutesDifference = Math.floor(difference / 1000 / 60);
+		difference -= minutesDifference * 1000 * 60
+
+		var secondsDifference = Math.floor(difference / 1000);
+
+		if (daysDifference >= 1) {
+			return daysDifference + ' d'
+		}
+		else if (hoursDifference >= 1 && hoursDifference < 24) {
+			return hoursDifference + ' hr'
+		}
+		else if (minutesDifference < 60 && minutesDifference >= 1) {
+			return minutesDifference + ' m';
+		}
+		else {
+			return secondsDifference + ' s'
+		}
+	}
 }
