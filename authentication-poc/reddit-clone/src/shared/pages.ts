c4f60edea1e599282adb7cb94bd24e19036c0dc8
Tommy Tran <trant28@mcmaster.ca>
2017-10-24 12:22:25 -0400

    Moved files to root

--- authentication-poc/reddit-clone/src/shared/pages.ts
+++
@@ -1,6 +0,0 @@
-export * from '../pages/about/about';
-export * from '../pages/search/search';
-export * from '../pages/subreddit/subreddit';
-export * from '../pages/home/home';
-export * from '../pages/login/login';
-export * from '../pages/tabs/tabs';
